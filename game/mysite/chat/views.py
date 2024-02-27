09e0bfdb52c54612bc6cb2b51573ad7edcab40c3
Michele <michele.toia30@gmail.com>
2024-02-27 14:17:44 +0100

    chat

---
+++ game/mysite/chat/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "chat/index.html")
+
+def room(request):
+    return render(request, "chat/room.html")
--- game/prova/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'index.html')
-
-def pong(request):
-    return render(request, 'pong.html')
