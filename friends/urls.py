4495e3606ae4aaceeebe296c1146d0b7a83bda46
mtoia <michele.toia30@gmail.com>
2024-03-25 18:36:26 +0100

    remove friend

--- friends/urls.py
+++ friends/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import send_friend_request, SendFriendRequestView, ListFriendRequestView, accept_friend_request, ListFriendsView, ex
+from .views import send_friend_request, SendFriendRequestView, ListFriendRequestView, accept_friend_request, ListFriendsView, ex, RemoveFriend
 
 urlpatterns = [
     # path('friend_request/', send_friend_request, name="friend_request")
     path('request/send/', SendFriendRequestView.as_view(), name='send'),
     path('request/list/', ListFriendRequestView.as_view(), name='list'),
     path('accept/<str:friend_request_id>/', accept_friend_request, name='accept'),
+    path('remove/', RemoveFriend.as_view(), name='remove'),
     path('list/', ListFriendsView.as_view()),
     path("", ex, name="index"),
 ]
