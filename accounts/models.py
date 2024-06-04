8fc662753e8ead88b87aa221cd0fb2798aa513e3
mtoia <michele.toia30@gmail.com>
2024-06-04 08:45:01 +0200

    fix exp, Cancel Friend Request

--- accounts/models.py
+++ accounts/models.py
@@ -36,7 +36,7 @@ class CustomUser(AbstractUser): # new
         total_wins = len([match for match in match_history if match['winner__username'] == self.username])
         total_loses = len([match for match in match_history if match['winner__username'] != self.username])
         total_exp = total_wins - total_loses
-        return total_exp
+        return max(total_exp, total_wins)
     
     def calculate_level(self):
         exp = {
